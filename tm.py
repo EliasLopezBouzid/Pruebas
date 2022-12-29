bbc507ebb9637ab0a469a532a69a7c7c1ad16660
Elias Lopez Bouzid <e.lopezbouzid@gmail.com>
2022-12-29 18:24:54 +0100

    New Files

--- tm.py
+++ tm.py
@@ -2,7 +2,7 @@ import argparse
 import datetime
 
 def add(args):
-    if args.priority == "":
+    if args.priority == None:
         args.priority = 'Normal'
     task = f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")};{args.priority};{args.name}'
     with open('task.csv', 'a') as f:
@@ -12,16 +12,21 @@ def list(args):
     with open('task.csv', 'r') as f:
         data = f.readlines()
         for line in data:
-            if args.priority in line:
+            if args.priority == None:
                 print(line)
-                
-        
+            else:
+                if args.priority in line:
+                    print(line)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('action', type=str, help='Especifique accion')
-    parser.add_argument('--name', type=str, help='Especifique nombre')
-    parser.add_argument('--priority', type=str, default='',choices=['Low', 'Normal', 'High'] help='Especifique prioridad (Low,Normal,High)
+    parser.add_argument('--name', type=str, help='Especifique nombre de tarea')
+    parser.add_argument('--priority',
+                        type=str,
+                        choices=['Low', 'Normal', 'High'],
+                        help='Especifique prioridad(Low,Normal,High)'
+                        )
     args = parser.parse_args()
     if args.action == "add":
         add(args)
